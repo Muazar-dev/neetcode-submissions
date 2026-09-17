c58f8b893867fc1c091bf05c908fb29b709d2bb1
Muazar <muazar.a.mohammed@gmail.com>
2026-09-16 22:58:01 -0500

    Add: is-anagram - submission-10

---
+++ Data Structures & Algorithms/is-anagram/submission-10.cs
@@ -0,0 +1,66 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        Dictionary<char, int> chars = new Dictionary<char, int>();
+        bool ana = true;
+        Dictionary<char, int> charss = new Dictionary<char, int>();
+
+        if(s.Length != t.Length) {
+            ana = false;
+            return ana;
+        }
+        
+        foreach(char a in s) {
+            if(!chars.ContainsKey(a)) {
+            chars.Add(a, 1);
+            }
+            else{
+                chars[a]++;
+            }
+        }
+
+        
+        foreach(char b in t) {
+            if(!charss.ContainsKey(b)) {
+            charss.Add(b, 1);
+            }
+            else{
+                charss[b]++;
+            }
+        }
+
+
+        foreach(var (key, value) in chars) {
+            if(charss.ContainsKey(key)) {
+                if(chars[key] != charss[key]) 
+                {
+                ana = false;
+                return ana;
+                }
+            }
+            else{
+            ana = false;
+            return ana;
+            }
+            
+
+        }
+
+        foreach(var (key, value) in charss) {
+            if(chars.ContainsKey(key)) {
+                if(chars[key] != charss[key]) 
+                {
+                ana = false;
+                return ana;
+                }
+            }
+            else{
+            ana = false;
+            return ana;
+            }
+            
+
+        }
+
+        return ana;
+    }
+}
